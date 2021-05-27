f4a675faaede9d8f7668331513b772e2cf4b32c4
nnemenzo9 <natnemenzo1@gmail.com>
2021-05-26 18:10:29 -0600

    fixed naive ms to follow paper

--- code/cluster.py
+++ code/cluster.py
@@ -6,33 +6,30 @@ from meanshift import MeanShift
 
 def cluster(data):
     # create meanshift object
-    ms = MeanShift('gaussian', True, True)
+    ms = MeanShift('gaussian')
 
     # do meanshift
     start = time.time()
-    results = ms.do(data, bandwidth = 2)
+    results = ms.do(data, bandwidth = 2, convergence_limit = 0.00001)
     end = time.time()
     print("elapsed time: ", end - start)
 
     # get results
-    original_points = results.original_points
-    shifted_points = results.shifted_points
-    modes = results.modes
-    groups = results.groups
+    query_set = results.query_set
+    reference_set = results.reference_set
 
     # show plot
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
     # show scatter plot
-    ax.scatter(x = original_points[:, 0], y = original_points[:, 1], s = 50, c = groups)
+    ax.scatter(x = reference_set[:, 0], y = reference_set[:, 1], s = 50, c = 'blue')
     ax.set_xlabel('x')
     ax.set_ylabel('y')
 
     # print shifted points
-    ax.scatter(x = shifted_points[:, 0], y = shifted_points[:, 1], s = 50, c = 'red', marker = 'X')
-    fig.savefig("ms result_working")
+    ax.scatter(x = query_set[:, 0], y = query_set[:, 1], s = 50, c = 'red', marker = 'X')
+    fig.savefig("../fig")
 
-
-data = np.genfromtxt('data.csv', delimiter = ',')
+data = np.genfromtxt('../test_data.csv', delimiter = ',')
 cluster(data)
--- code/meanshift.py
+++ code/meanshift.py
@@ -5,107 +5,51 @@ import utils
 CONVERGENCE_LIMIT = 0.000001
 GROUPING_DISTANCE = 0.00001
 
-# Class that implements mean-shift algorithm
-# TODO: Optimize
-# optimization: http://proceedings.mlr.press/v2/wang07d/wang07d.pdf
+# Class that implements naive mean-shift algorithm
+# pern: http://proceedings.mlr.press/v2/wang07d/wang07d.pdf
 class MeanShift:
     # __init__(): Constructor for MeanShift class, takes in the kernel for the algorithm
-    def __init__(self, kernel, need_modes, need_groups):
+    def __init__(self, kernel):
         if kernel == 'gaussian':
             self.kernel = utils.gaussian_kernel_ms
         if kernel == 'multivariate gaussian':
             self.kernel = utils.gaussian_kernel_correlations
-        self.need_modes = need_modes
-        self.need_groups = need_groups
 
     # do(): Perform the mean shift algorithm to find the modes
     # data is of the form: [ features ]
     #                      [ features ] <- records
-    def do(self, data, bandwidth):
-        # Copy original points
-        original_data = data
+    def do(self, reference_set, bandwidth, convergence_limit):
+        # in this case, query set == reference set
+        query_set = np.array(reference_set)
 
-        # Convert data to np array
-        working_data = np.array(data)
+        # distance vector
+        distances = [float('inf')] * len(reference_set)
 
-        # Keep track of which points have converged
-        convergence_array = [False] * len(data)
+        while max(distances) > convergence_limit:
+            for point in range(len(query_set)):
+                # save old point
+                old_point = np.array(query_set[point])
 
-        # when smallest diff of shift is less than convergence limit, all have converged
-        counter = 0
-        max_diff = float('inf')
-        while max_diff > CONVERGENCE_LIMIT:
-            max_diff = 0
-            for point in range(len(working_data)):
-                if not convergence_array[point]: 
-                    temp_pt = working_data[point]
+                # reassign point
+                query_set[point] = self.__compute_mean_shift(query_set[point], reference_set, bandwidth)
 
-                    # compute mean shift for point
-                    mean_shift = self.__compute_mean_shift(temp_pt, working_data, bandwidth) # compute m(x) - x
-                    new_pt = temp_pt + mean_shift # x = m(x)
+                # compute distance
+                distances[point] = np.linalg.norm(query_set[point] - old_point)
 
-                    # check for convergence
-                    diff = np.linalg.norm(new_pt - temp_pt)
-                    if diff < CONVERGENCE_LIMIT:
-                        convergence_array[point] = True
-
-                    # get smallest diff
-                    if diff > max_diff:
-                        max_diff = diff
-
-                    # reassign point
-                    working_data[point] = new_pt
-
-        # find modes 
-        modes = self.__get_modes(working_data) if self.need_modes else None
-
-        # put points into groups
-        groups = self.__group_points(working_data, modes) if self.need_groups else None
-
-        return MeanShiftResult(modes, original_data, working_data, groups)
-
+        return MeanShiftResult(reference_set, query_set)
 
     # __compute_mean_shift(): Computes mean shift vector m(x) - x 
     # per https://en.wikipedia.org/wiki/Mean_shift
-    def __compute_mean_shift(self, x, data, bandwidth):
+    def __compute_mean_shift(self, x_q, reference_set, bandwidth):
         # create weights array
-        weights = np.array([self.kernel((np.linalg.norm(x - x_i) ** 2) / (bandwidth ** 2), bandwidth) for x_i in data])
+        weights = np.array([self.kernel((np.linalg.norm(x_q - x_r) ** 2) / (bandwidth ** 2), bandwidth) for x_r in reference_set])
 
         # matmul weights by data
-        numerator = np.matmul(data.T, weights)
-        return (numerator / weights.sum()) - x
-        
-    # __get_modes(): Find modes based on euclidian distance, all points should be converged before this is called
-    def __get_modes(self, data):
-        modes = []
-
-        # append the first mode
-        modes.append(data[0])
-        for point in range(len(data)):
-            add = True 
-            for mode in modes:
-                # there is already a group for that point
-                if (np.linalg.norm(data[point] - mode) <= GROUPING_DISTANCE):
-                    add = False
-                    break
-            if add:
-                modes.append(data[point])
-        return np.array(modes)
-
-    # __group_points(): Groups points based on euclidian distance from modes, points must be converged before this is called
-    def __group_points(self, data, modes):
-        modes = np.array(modes)
-        groups = [None] * len(data)
-        for mode_index in range(len(modes)):
-            for point in range(len(data)):
-                if np.linalg.norm(data[point] - modes[mode_index]) <= GROUPING_DISTANCE:
-                    groups[point] = mode_index
-        return groups
+        numerator = np.matmul(reference_set.T, weights)
+        return numerator / weights.sum()
 
 # Class that returns result from a mean-shift algorithm
 class MeanShiftResult:
-    def __init__(self, modes, original_points, shifted_points, point_groups):
-        self.modes = modes
-        self.original_points = original_points
-        self.shifted_points = shifted_points
-        self.groups = point_groups
+    def __init__(self, reference_set, query_set):
+        self.reference_set = reference_set
+        self.query_set = query_set
